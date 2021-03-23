6d46eb359158ff23d12cffe27d0a2ee7f14afbb1
임태현 <noeulnight.mail@gmail.com>
2021-03-23 13:40:09 +0900

    hotfix: :(

--- src/web/template/bs3/js.php
+++ src/web/template/bs3/js.php
@@ -2,7 +2,7 @@
 <script src="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE/"?>jquery.min.js"></script>
 
 <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE/"?>bootstrap.min.js"></script>
 
 <?php
 if(file_exists("./admin/msg.txt"))
